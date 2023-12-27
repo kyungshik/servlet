530596e4638a38c6753c056aa318e91780f232a5
Kyungshik <kyungshikseong@gmail.com>
2023-12-27 12:12:12 +0900

    Servlet 요청과 응답

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -13,7 +13,17 @@ public class HelloServlet extends HttpServlet {
 
     //해당 Servlet이 호출되면 service 메서드가 호출된다.
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("HelloServlet.service"); 
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        //servlet이 없다면 http 응답 메시지를 모두 파싱해서 알아봐야 한다. servlet의 편리함
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
     }
 }
